a3e12fbf48a1347d5f6da0aa7a5a48b1ae33ed9d
Blake Davies <bbd@users.campus>
2020-02-10 17:14:37 -0600

    got everything ready for milestone 3

---
+++ Data/Drink.cs
@@ -0,0 +1,49 @@
+﻿/*
+
+* Author: Blake Davies
+
+* Class name: Drink.cs
+
+* Purpose: to create the Drink base class
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// default drink class
+    /// </summary>
+    public abstract class Drink
+    {
+
+        /// <summary>
+        /// the price of the drink
+        /// </summary>
+        public abstract double Price { get; }
+
+        /// <summary>
+        /// the amount of calories inside the drink
+        /// </summary>
+        public abstract uint Calories { get; }
+
+        /// <summary>
+        /// the ingredients inside the drink
+        /// </summary>
+        public abstract List<string> Ingredients { get; }
+
+        /// <summary>
+        /// the size of the drink
+        /// </summary>
+        public Size Size { get; set; } = Size.Small;
+
+        /// <summary>
+        /// weather the drink contains ice
+        /// </summary>
+        public bool Ice { get; set; } = true;
+
+
+    }
+}
---
+++ Data/SodaFlavor.cs
@@ -0,0 +1,27 @@
+﻿/*
+
+* Author: Blake Davies
+
+* Class name: SodaFlavor.cs
+
+* Purpose: to create the SodaFlavor enum
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// all possible soda flavors
+    /// </summary>
+    public enum SodaFlavor
+    {
+        CreamSoda,
+        OrangeSoda,
+        Sarsaparilla,
+        BirchBeer,
+        RootBeer
+    }
+}
