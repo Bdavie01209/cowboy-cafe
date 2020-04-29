f52103b9247c6b6b0151018629936abfa89ee364
Blake Davies <bbd@ksu.edu>
2020-04-28 20:16:01 -0500

    added some things didn't add some thingsz

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CowboyCafe.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -17,8 +18,45 @@ namespace Website.Pages
             _logger = logger;
         }
 
+        public string SearchTerms { get; set; }
+
+        public int MinCalories { get; set; }
+        public int MaxCalories { get; set; }
+        public int PriceMin { get; set; }
+        public int PriceMax { get; set; }
+        public bool EntreeSelected { get; set; }
+        public bool DrinkSelected { get; set; }
+        public bool SideSelected { get; set; }
+
         public void OnGet()
         {
+            SearchTerms = Request.Query["SearchTerms"];
+            int d = 0;
+            if(int.TryParse(Request.Query["MinCalories"], out d))
+            {
+                MinCalories = d;
+            }
+            int f = 0;
+            if(int.TryParse(Request.Query["MaxCalories"], out f))
+            {
+                MaxCalories = f;
+            }
+            foreach(string s in Request.Query["TypeFood"])
+            {
+                if (s.Equals("Entree"))
+                {
+                    EntreeSelected = true;
+                }
+                if (s.Equals("Side"))
+                {
+                    SideSelected = true;
+                }
+                if (s.Equals("Drink"))
+                {
+                    DrinkSelected = true;
+                }
+            }
+
 
         }
     }
